f63f5ea3c4d724085d06c10018b5279eed7ac5ff
Bin Zhang <sjtuzb@gmail.com>
2015-04-20 14:02:57 +0800

    add gateway_lib

--- paho/br/test.py
+++ paho/br/test.py
@@ -3,9 +3,10 @@
 
 # test.py
 
-from get_config import *
+from gateway_lib import *
 
-test = GetConfig()
-broker = test.get_broker()
-print "get broker"
-print broker
+test_data = [{"device": "test", "timestamp": 1428928106333, "id": 3, "minimum": 1, "average": 2, "maximum": 3}, {"device": "test", "timestamp": 1428928106366, "id": 4, "minimum": 2, "average": 4, "maximum": 6}]
+
+gateway_config()
+
+gateway_puber(test_data)
